842cc38d89a126ca5f0048225dc82a498aee4e44
mn0803 <watervalley83@gmail.com>
2025-09-25 13:24:27 +0900

    section9

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { Practice1 } from "./Practices/Practice1";
+import { Practice2 } from "./Practices/Practice2";
+import { Practice3 } from "./Practices/Practice3";
 import "./styles.css";
+import React from "react";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+     <Practice1 />
+     <Practice2 />
+     <Practice3 />
     </div>
   );
 }
